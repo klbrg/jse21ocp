beed9633a51166e287aefe606555268812e59655
klbrg <48217039+klbrg@users.noreply.github.com>
2025-06-07 20:25:18 +0200

    refactor: improve comments and organization in Point2D class

--- part2/ch01/Point2D.java
+++ part2/ch01/Point2D.java
@@ -1,8 +1,9 @@
 // File: Point2D.java
 public class Point2D {
-    // Class Member Decalartions
+    // Static variable:
+    private static String info = "A 2D point represented by (x,y)-coordinates.";
 
-    // Fields:
+    // Instance variables:
     private int x; // The x-coordinate
     private int y; // The y-coordinate
 
@@ -12,7 +13,7 @@ public class Point2D {
         y = yCoord;
     }
 
-    // Methods:
+    // Instance methods:
     public int getX() {
         return x;
     }
@@ -31,5 +32,17 @@ public class Point2D {
 
     public String toString() {
         return "(" + x + "," + y + ")"; // Format: (x,y)
+    }
+
+    // Static methods:
+    public static double distance(Point2D p1, Point2D p2) {
+        int xDiff = p1.x - p2.x;
+        int yDiff = p1.y - p2.y;
+
+        return Math.sqrt(xDiff * xDiff + yDiff + yDiff);
+    }
+
+    public static void showInfo() {
+        System.out.println(info);
     }
 }
