4cbafc6481f25289a724a63ada3c23026defe9ae
klbrg <48217039+klbrg@users.noreply.github.com>
2025-06-08 11:07:06 +0200

    feat(part2/ch02): enhance output formatting for One's and Two's complement

--- part2/ch02/src/Complements.java
+++ part2/ch02/src/Complements.java
@@ -4,9 +4,10 @@ public class Complements {
     public static void main(String[] args) {
         String regex = "(.{4})(?!$)"; // Group by 4s
         System.out.printf("%40s (%4d)%n", Integer.toBinaryString(mask).replaceAll(regex, "$1 "), mask);
-        System.out.printf("%40s (%d) One's complement%n", Integer.toBinaryString(~mask).replaceAll(regex, "$1 "),
+        System.out.printf("%40s (%d) One's complement (~N)%n", Integer.toBinaryString(~mask).replaceAll(regex, "$1 "),
                 ~mask);
-        System.out.printf("%40s (%d) Two's complement%n", Integer.toBinaryString(~mask + 1).replaceAll(regex, "$1 "),
+        System.out.printf("%40s (%d) Two's complement (~N + 1)%n",
+                Integer.toBinaryString(~mask + 1).replaceAll(regex, "$1 "),
                 ~mask + 1);
 
     }
